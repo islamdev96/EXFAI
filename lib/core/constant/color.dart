cdc1e19fa4527c7d88570ab7c23f347fe065106d
islamdev96 <islamsayedglab@gmail.com>
2024-02-02 14:14:51 +0200

    ..

--- lib/core/constant/color.dart
+++ lib/core/constant/color.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
+import '../../all_export.dart';
 
 class AppColor {
-  // Grayscale Colors
   static const Color test = Color(0xFFF60505);
   static const Color black = Color(0xFF000000);
   static const Color white = Color(0xFFFFFFFF);
@@ -15,17 +14,19 @@ class AppColor {
   static const Color secondaryBackground = Color(0xFFDBDBED);
   static const Color cardBackground = Color(0xFFFFFFFF);
 
-  // Button Colors
   static const Color primaryButton = Color(0xFF808080);
   static const Color secondaryButton = Color(0xFF2196F3);
 
-  // Border Colors
   static const Color primaryBorder = Color(0xFF0F1169);
   static const Color secondaryBorder = Color(0xFF808080);
 
-  // Alert Colors
   static const Color success = Color(0xFF00C853);
   static const Color error = Color(0xFFD32F2F);
   static const Color warning = Color(0xFFFFAB00);
   static const Color info = Color(0xFF2196F3);
+
+  // Harmonious Colors
+  static const Color harmony1 = Color(0xFF4CAF50); // Green
+  static const Color harmony2 = Color(0xFF9C27B0); // Purple
+  static const Color harmony3 = Color(0xFFFF9800); // Orange
 }
