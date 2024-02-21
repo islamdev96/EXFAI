c9a33992c010f796824becbf74d115c8f8b4b5c8
islamdev96 <islamsayedglab@gmail.com>
2024-02-22 01:58:33 +0200

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends GetWidget<LocaleController> {
     return ScreenUtilInit(
       useInheritedMediaQuery: true,
       designSize: const Size(360, 690),
+      //
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
